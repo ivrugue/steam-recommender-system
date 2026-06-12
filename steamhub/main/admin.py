aa82e0ddb64286fdec98481b564720ddb3c85a1c
Iván <ivrugue@gmail.com>
2026-06-12 21:14:39 +0200

    Add models

--- steamhub/main/admin.py
+++ steamhub/main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from main.models import *
 
 # Register your models here.
+admin.site.register(Desarrollador)
+admin.site.register(Etiqueta)
+admin.site.register(Juego)
+admin.site.register(Metadata)
--- steamhub/main/models.py
+++ steamhub/main/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Desarrollador(models.Model):
+    nombre = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Etiqueta(models.Model):
+    nombre = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Juego(models.Model):
+    juego_id = models.IntegerField(primary_key=True)
+    titulo = models.CharField(max_length=200, unique=True)
+    link = models.URLField()
+    imagen = models.URLField()
+    fecha = models.DateField(null=True, blank=True)
+    peak_jugadores = models.IntegerField()
+    precio = models.FloatField(null=True, blank=True)
+    desarrolladores = models.ManyToManyField(Desarrollador)
+    etiquetas = models.ManyToManyField(Etiqueta)
+    usuarios_gustan = models.ManyToManyField(User, related_name="juegos_gustados")
+
+    def __str__(self):
+        return self.titulo
+    
+
+class Metadata(models.Model):
+    ultima_carga = models.DateTimeField()
+
+    def __str__(self):
+        return str(self.ultima_carga)
